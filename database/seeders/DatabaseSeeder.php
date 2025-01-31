239bf448d3e32c00608863f9379540a7bfeff07b
Karl Murray <karl.murray@luminfire.com>
2025-01-31 13:50:07 -0600

    Add User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,26 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('secret'),
+            'role' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Super Admin',
+            'email' => 'super-admin@example.com',
+            'password' => bcrypt('secret'),
+            'role' => 'super_admin',
         ]);
     }
 }
